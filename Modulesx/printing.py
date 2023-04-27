c2e4ffa6f7502a14061f4acde221fcb59e85dd16
Udemezue Iloabachie <udemezue@gmail.com>
2023-04-26 21:47:52 -0400

    enhancements to code breaker game

--- Modulesx/printing.py
+++ Modulesx/printing.py
@@ -1,31 +1,46 @@
 import time
 
-def printing(text, delay=0.05, style='letter', new_line=True): 
-    """printing style"""
-    if style.lower() == 'letter':   
-        for _ in range(len(text)):
-            print(text[:_ + 1], end='\r')
-            time.sleep(delay)
-        if new_line: print()
-    elif style.lower() == 'word':
-        text = text.split(' ')
-        for _ in range(len(text)):
-            print(' '.join(text[:_ + 1]), end='\r')
-            time.sleep(delay)
-        if new_line: print()
+def printing(text, delay=0.05, style='letter', new_line=True, rev=False): 
+    """animated text printing"""
+    match rev:
+        case False:
+            if style.lower() == 'letter':   
+                for _ in range(len(text)):
+                    print(text[:_ + 1], end='\r')
+                    time.sleep(delay)    
+            elif style.lower() == 'word':
+                text = text.split(' ')
+                for _ in range(len(text)):
+                    print(' '.join(text[:_ + 1]), end='\r')
+                    time.sleep(delay)
+        case True:
+            if style.lower() == 'letter':  
+                for _ in range(len(text)):
+                    print(text[-1 - _:], end='\r')
+                    time.sleep(delay)
+            elif style.lower() == 'word':
+                text = text.split(' ')
+                for _ in range(len(text)):
+                    print(' '.join(text[-1 - _:]), end='\r')
+                    time.sleep(delay)
+    if new_line: print()
         
+def prt(text):
+    for x in text:
+        print(x, end="", flush=True)
+        time.sleep(0.05)
 
 
+# Code test
 match __name__:
     case "__main__": 
-        printing("word by word printing for udemezue iloabachie", style='word', delay=0.5)
-        printing("letter by letter printing for udemezue iloabachie", delay=0.06)
+        texts = "CLOSE: at least one correct digit but in wrong position"
+        printing("word by word printing for udemezue iloabachie", style='word', delay=0.5, rev=True)
+        printing("letter by letter prin#####\b\b\b\b\bting for udemezue iloaba\bchie with new line false", 0.06, 'letter', False, True)
+        print()        
+        prt("CLOSE: at least one correct digit but in wrong position")
+        print()
+        printing(texts, rev=True)
 
 
-        printing("{:^57}".format('Welcome to CODE BREAKER')), time.sleep(1.3)
-        printing("""=========================================================
-                            ***CLUES***
-        MATCH: at least one correct digit in the correct position
-        CLOSE: at least one correct digit but in wrong position
-        NOPE:  no correct digit in guess
-        ========================================================="""), time.sleep(1)
+
--- xCodeCracker/code_cracker.py
+++ xCodeCracker/code_cracker.py
@@ -5,19 +5,32 @@ import os
 
 os.system('cls')
 
-def printing(text, delay=0.05, new_line=True):
-    for _ in range(len(text)):
-        print(text[:_ + 1], end='\r'), time.sleep(delay)
+def printing(text, delay=0.07, new_line=True, rev=False):
+    if not rev:
+        for _ in range(len(text)):
+            print(text[:_ + 1], end='\r')
+            time.sleep(delay)
+    if rev:    
+        for _ in range(len(text)):
+            print(text[-1 - _:], end='\r')
+            time.sleep(delay)
     if new_line: print()
 
 print()
-printing("{:^57}".format('Welcome to CODE BREAKER')), time.sleep(0.5)
-print("""=========================================================
-                    ***CLUES***
-MATCH: at least one correct digit in the correct position
-CLOSE: at least one correct digit but in wrong position
-NOPE:  no correct digit in guess
-=========================================================\n"""), time.sleep(1)
+for _ in range(3):
+    if _ == 0: printing("{:^57}".format('Welcome to CODE BREAKER'), new_line=False, rev=True), time.sleep(0.3)
+    if _ == 0: print(" " * 57, end='\r'), time.sleep(0.2)
+    print("{:^57}".format('Welcome to CODE BREAKER'), end='\r'), time.sleep(0.5)
+    if _ != 2: print(" " * 57, end='\r'), time.sleep(0.2)
+print()
+print("=" * 57), time.sleep(0.1)
+print("{:^57}".format("***CLUES***")), time.sleep(0.1)
+print("MATCH: at least one correct digit in the correct position"), time.sleep(0.1)
+print("CLOSE: at least one correct digit but in wrong position"), time.sleep(0.1)
+print("NOPE:  no correct digit in your guess"), time.sleep(0.1)
+print("=" * 57, end='\r'), time.sleep(0.1)
+print(" " * 57, end='\r'), time.sleep(0.2)
+print("=" * 57 + "\n"), time.sleep(1)
 
 # Captures player name
 printing('Enter your name: ', new_line=False)
@@ -26,7 +39,8 @@ while True:
     if player.isalpha() and len(player) <= 8:
         player = player.capitalize()
         print()
-        printing(f'Welcome to Code Breaker {player}')
+        animate = '#' * len(player) + '\b' * len(player)
+        printing(f'Welcome Code Breaker {animate}{player}    ')
         break
     else:
         print("Invalid name. Must be max 8 characters and letters only\n")
@@ -62,11 +76,12 @@ except Exception:
     high_score = best_time = float('inf') 
 
 count = 1 # Sets the counter variable for counting steps
+used_hint = False
 
 def display_records():
-    print("\n{:>34}".format('**Leader Board**'))
-    print('+----------+' + '---------------------+' * 2)
-    print('| {:8} | {:19} | {:19} |'.format('Game', 'Steps Record', 'Time Record'))
+    print("\n{:>34}".format('**Leader Board**')), time.sleep(0.05)
+    print('+----------+' + '---------------------+' * 2), time.sleep(0.05)
+    print('| {:8} | {:19} | {:19} |'.format('Game', 'Steps Record', 'Time Record')), time.sleep(0.05)
     keys = ("TWO", "THREE", "FOUR")
     for key in keys:
         try:
@@ -76,15 +91,15 @@ def display_records():
             name1 = name2 = "--"
             high_score = best_time = 0
         minute, sec = divmod(best_time, 60)  
-        print('+----------' * 5 + '+')
-        print('| {:8} | {:8} |{:9d} | {:8} |{:6d}:{:02d} |'.format(key, name1, high_score, name2, minute, sec))
-    print('+----------' * 5 + '+\n')
+        print('+----------' * 5 + '+'), time.sleep(0.05)
+        print('| {:8} | {:8} |{:9d} | {:8} |{:6d}:{:02d} |'.format(key, name1, high_score, name2, minute, sec)), time.sleep(0.05)
+    print('+----------' * 5 + '+\n'), time.sleep(0.05)
 
-display_records()   
-    
-game_on = input("Press 'Enter' to start or type 'reset' to clear score record: ")  # Ensures that timer only starts counting when player is ready
+display_records()  
 
-if game_on.lower() == 'reset':
+reset = input("Would you like to reset the Leader board? y or n: ") 
+
+if reset.lower() == 'y':
     confirm = input("Are you sure? y or n: ")
     if confirm == 'y':
         keys = ("TWO", "THREE", "FOUR")
@@ -93,51 +108,66 @@ if game_on.lower() == 'reset':
             records["best_times"][rkey] = ['--', 0]
         with open(r'xCodeCracker/records.json', 'w') as file:
             json.dump(records, file, indent=2, sort_keys=True)
-        print('Records cleared...')     
+        print('Records cleared...') 
+        display_records() 
     else:
         print('\nRecords unchanged')
-    display_records()    
-        
-else:
-    # Game core
-    start = time.time()  # Sets start time
-    while True:
-        attempt = "attempt" if count == 1 else "attempts" 
-        guess = input(f"\nMake a {num_digits}-digit number guess: ")    
-        if guess.isnumeric() and len(guess) == num_digits and len(set(guess)) == num_digits:
-            merge = [len(set(_)) for _ in zip(pc_code, guess)]
-            if guess == pc_code:
-                break        
-            elif 1 in merge:
-                print(f"MATCH: {attempt}={count}")            
-            elif len(guess + pc_code) != len(set(guess + pc_code)):  
-                print(f"CLOSE: {attempt}={count}")           
-            else:
-                print(f"NOPE: {attempt}={count}")
-            count += 1
-        elif guess.lower() == 'quit':
-            print("\nExiting game...")
-            break
+    
+
+# Game core
+game_on = input("\nPress 'Enter' to start Code Cracking: ")  # Ensures that timer only starts counting when player is ready
+
+start = time.time()  # Sets start time
+while True:
+    attempt = "attempt" if count == 1 else "attempts" 
+    guess = input(f"\nMake a {num_digits}-digit number guess or type hint or quit: ")    
+    if guess.isnumeric() and len(guess) == num_digits and len(set(guess)) == num_digits:
+        merge = [len(set(_)) for _ in zip(pc_code, guess)]
+        if guess == pc_code:
+            break        
+        elif 1 in merge:
+            print(f"MATCH: {attempt}={count}")            
+        elif len(guess + pc_code) != len(set(guess + pc_code)):  
+            print(f"CLOSE: {attempt}={count}")           
         else:
-            print(f"Guess Error, guess must contain {num_digits} distinct numbers")
+            print(f"NOPE: {attempt}={count}")
+        count += 1
+    elif guess.lower() == 'hint' and not used_hint:
+        hint_index = random.randint(0, num_digits - 1) 
+        print("|", end="")
+        for _ in range(num_digits):                
+            if _ != hint_index:
+                print("_", end="|")
+            else:
+                print(pc_code[_], end="|")
+        print()
+        used_hint = True            
+    elif guess.lower() == 'hint' and used_hint:
+        print("Sorry, you have used your hint")                
+    elif guess.lower() == 'quit':
+        print()
+        printing("Exiting game...")
+        break
+    else:
+        print(f"Guess Error, guess must contain {num_digits} distinct numbers")
 
-    if guess.lower() != 'quit':
-        end = round(time.time() - start)  # Captures end time
-        minute, sec = divmod(end, 60)
-        seconds = '{:02d}'.format(sec)
-        print(f"\n***CODE CRACKED***\nCompleted in {count} {attempt} and took {minute} minute(s) and {seconds} second(s)")
+if guess.lower() != 'quit':
+    end = round(time.time() - start)  # Captures end time
+    minute, sec = divmod(end, 60)
+    seconds = '{:02d}'.format(sec)
+    print(f"\n***CODE CRACKED***\nCompleted in {count} {attempt} and took {minute} minute(s) and {seconds} second(s)")
 
-        # Saves only the fastest score or time in JSON file    
-        with open(r'xCodeCracker/records.json', 'w') as file:
-            also = "ALSO " if (high_score == 0 or count < high_score) and (best_time == 0 or end < best_time) else ""
-            if high_score == 0 or count < high_score:
-                high_score = count 
-                records["high_scores"][key] = [player, high_score]
-                printing(f"**Congratulations {player}!!! You broke the steps record for '{key}'")  
-            if best_time == 0 or end < best_time:
-                best_time = end 
-                records["best_times"][key] = [player, best_time]
-                printing(f"**Congratulations {player}!!! You {also}broke the time record for '{key}'")  
-            json.dump(records, file, indent = 2, sort_keys=True)  
+    # Saves only the fastest score or time in JSON file    
+    with open(r'xCodeCracker/records.json', 'w') as file:
+        also = "ALSO " if (high_score == 0 or count < high_score) and (best_time == 0 or end < best_time) else ""
+        if high_score == 0 or count < high_score:
+            high_score = count 
+            records["high_scores"][key] = [player, high_score]
+            printing(f"**Congratulations {animate}{player}!!! You broke the steps record for '{key}'")  
+        if best_time == 0 or end < best_time:
+            best_time = end 
+            records["best_times"][key] = [player, best_time]
+            printing(f"**Congratulations {animate}{player}!!! You {also}broke the time record for '{key}'")  
+        json.dump(records, file, indent = 2, sort_keys=True)  
 
-    display_records()
+display_records()
