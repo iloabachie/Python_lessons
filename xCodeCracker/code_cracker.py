baa822bbcc6fcb9b0979b447839895b879e9ceb0
Udemezue Iloabachie <udemezue@gmail.com>
2023-04-28 16:13:25 -0400

    Code structure and game order arrangement

--- xCodeCracker/code_cracker.py
+++ xCodeCracker/code_cracker.py
@@ -3,8 +3,6 @@ import json
 import time
 import os
 
-os.system('cls')
-
 def printing(text, delay=0.07, new_line=True, rev=False):
     if not rev:
         for _ in range(len(text)):
@@ -23,10 +21,11 @@ def flashprint(text, flashes=5, delay=0.2, stay=True):
     if stay: print(text)
 
 def display_records():
-    print("\n{:>34}".format('**Leader Board**')), time.sleep(0.05)
+    print()
+    print("{:>34}".format('**Leader Board**')), time.sleep(0.05)
     print('+----------+' + '---------------------+' * 2), time.sleep(0.05)
     print('| {:8} | {:19} | {:19} |'.format('Game', 'Steps Record', 'Time Record')), time.sleep(0.05)
-    keys = ("TWO", "THREE", "FOUR")
+    keys = ("2-DIGITS", "3-DIGITS", "4-DIGITS")
     for key in keys:
         try:
             name1, high_score = records["high_scores"][key]
@@ -38,7 +37,9 @@ def display_records():
         print('+----------' * 5 + '+'), time.sleep(0.05)
         print('| {:8} | {:8} |{:9d} | {:8} |{:6d}:{:02d} |'.format(key, name1, high_score, name2, minute, sec)), time.sleep(0.05)
     print('+----------' * 5 + '+\n'), time.sleep(0.05)
-    
+
+os.system('cls')
+
 print()
 printing("{:^57}".format('Welcome to CODE BREAKER'), delay=0.1, new_line=False, rev=True), time.sleep(0.3)
 flashprint("{:^57}".format('Welcome to CODE BREAKER'), delay=0.4, flashes=3), time.sleep(0.5)
@@ -49,10 +50,15 @@ print("MATCH: at least one correct digit in the correct position"), time.sleep(0
 print("CLOSE: at least one correct digit but in wrong position"), time.sleep(0.2)
 print("NOPE:  no correct digit in your guess"), time.sleep(0.2)
 flashprint("=" * 57, flashes=1), time.sleep(1)
-print()
 
-# Captures player name
-printing('Enter your name: ', new_line=False)
+try: # Attempts to extract the records dictionary from JSON file
+    with open(r'xCodeCracker/records.json', 'r') as file:
+        records = json.load(file)
+except Exception:  # Sets high score and best time to infinity if record does not exist or JSON file absend
+    records = {"high_scores": {}, "best_times": {}} # Sets the dictionary of the records for first time play
+display_records()
+
+printing('Enter your name: ', new_line=False) # Captures player name
 while True:
     player = input('Enter your name: ')
     if player.isalpha() and len(player) <= 8:
@@ -63,48 +69,13 @@ while True:
         break
     else:
         print("Invalid name. Must be maximum 8 characters and contain only letters\n")
-        
-# To get the number of digits of the Code to crack
-while True:
-    num_digits = input("\nEnter number of digits to crack: ")
-    if num_digits.isnumeric() and 2 <= int(num_digits) <= 4:
-        num_digits = int(num_digits)
-        break
-    else:
-        print("Error. Choose a number between 2 and 4 inclusive")    
-
-# Sets the key value for keeping high score
-match num_digits:
-    case 2: key = 'TWO'
-    case 3: key = 'THREE'
-    case 4: key = 'FOUR'
-
-# Picks a random code with the number of specified digits       
-pc_code = "".join(random.sample([str(_) for _ in range(10)], num_digits))
-
-# Sets the dictionary of the records for first time play
-records = {"high_scores": {}, "best_times": {}}
-
-try: # Attempts to extract the records dictionary from JSON file
-    with open(r'xCodeCracker/records.json', 'r') as file:
-        records = json.load(file)
-        high_score = records["high_scores"][key][1]
-        best_time = records["best_times"][key][1]
-except Exception:
-    # Sets high score and best time to infinity if record does not exist or JSON file absend
-    high_score = best_time = float('inf') 
-
-count = 1 # Sets the counter variable for counting steps
-used_hint = False
-
-display_records()  
-
+print()
+       
 reset = input("Would you like to reset the Leader board? y or n: ") 
-
 if reset.lower() == 'y':
     confirm = input("  Are you sure? y or n: ")
     if confirm == 'y':
-        keys = ("TWO", "THREE", "FOUR")
+        keys = ("2-DIGITS", "3-DIGITS", "4-DIGITS")
         for rkey in keys:
             records["high_scores"][rkey] = ['--', 0]
             records["best_times"][rkey] = ['--', 0]
@@ -117,12 +88,35 @@ if reset.lower() == 'y':
         print('\nRecords unchanged\n')    
 else:
     print('\nRecords unchanged\n')  
+time.sleep(1)
+
+# To get the number of digits of the Code to crack
+while True:
+    num_digits = input("Enter number of digits to crack: ")
+    if num_digits.isnumeric() and 2 <= int(num_digits) <= 4:
+        num_digits = int(num_digits)
+        break
+    else:
+        print("Error. Choose a number between 2 and 4 inclusive")    
+
+match num_digits: # Sets the key value for KEY
+    case 2: key = '2-DIGITS'
+    case 3: key = '3-DIGITS'
+    case 4: key = '4-DIGITS'
 
 # Game core
-time.sleep(1)
-input("Press 'Enter' to start Code Cracking: ")  # Ensures that timer only starts counting when player is ready
+try:
+    high_score = records["high_scores"][key][1]
+    best_time = records["best_times"][key][1]
+except:
+    high_score = best_time = float('inf')
+count = 1 # Sets the counter variable for counting steps
+used_hint = False
+pc_code = "".join(random.sample([str(_) for _ in range(10)], num_digits))
+start = time.time()  # Sets start time   
 
-start = time.time()  # Sets start time
+input("\nPress 'Enter' to start Code Cracking: ")  # Ensures that timer only starts counting when player is ready
+
 while True:
     attempt = "attempt" if count == 1 else "attempts"     
     if not used_hint:
@@ -158,13 +152,13 @@ while True:
     elif guess.lower() == 'hint' and used_hint:
         print("Sorry, you have used your hint") 
     elif guess.lower() == 'python' and used_hint:
-        used_hint = False                   
+        used_hint = False  
+    elif guess.lower() == 'test':
+        flashprint(pc_code, delay=0.5, stay=False, flashes=1)
     elif guess.lower() == 'quit':
         print()
         printing("Exiting game...")
         break
-    elif guess.lower() == 'test':
-        flashprint(pc_code, delay=0.5, stay=False, flashes=1)
     else:
         print(f"Guess Error, guess must contain {num_digits} distinct numbers")
 
@@ -174,6 +168,7 @@ if guess.lower() != 'quit':
     second = '{:02d}'.format(sec)
     minute1 = "minute" if minute in [0, 1] else "minutes" 
     second1 = "second" if sec in [0, 1] else "seconds" 
+    
     print()
     flashprint("     ***CODE CRACKED***", flashes=4)
     printing(f"Completed in {count} {attempt} and took {minute} {minute1} and {second} {second1}")
@@ -183,6 +178,7 @@ if guess.lower() != 'quit':
         records["high_scores"][key] = [player, count]         
     if (best_time == 0 or end < best_time):
         records["best_times"][key] = [player, end]
+        
     if (high_score == 0 or count < high_score) and (best_time == 0 or end < best_time): 
         printing(f"MASTER CODE BREAKER!!! {player}, you SMASHED the steps and time records for '{key}'")
     else:
